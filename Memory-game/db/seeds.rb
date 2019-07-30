89fbdb7d8899318c8de5f00042c17fe4dafc4118
Natany Caxeta <natanycaxeta@icloud.com>
2019-07-29 20:54:43 -0400

    new seed

--- Memory-game/db/seeds.rb
+++ Memory-game/db/seeds.rb
@@ -7,12 +7,12 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 
-card1 = Card.create(card_image:"https://i.imgur.com/nJomRpY.jpg")
-card2 = Card.create(card_image:"https://i.imgur.com/ydJDg9a.jpg")
-card3 = Card.create(card_image:"https://i.imgur.com/eOsUJfm.jpg")
-card4 = Card.create(card_image:"https://i.imgur.com/wCnslyh.jpg")
-card5 = Card.create(card_image:"https://i.imgur.com/a25NZFa.jpg")
-card6 = Card.create(card_image:"https://i.imgur.com/yJixmxW.jpg")
-card7 = Card.create(card_image:"https://i.imgur.com/hq8nbWc.jpg")
-card8 = Card.create(card_image:"https://i.imgur.com/S7lojQF.jpg")
+card1 = Card.create(card_image:"https://imgur.com/4bMjr4Z")
+card2 = Card.create(card_image:"https://imgur.com/adfVNEO")
+card3 = Card.create(card_image:"https://imgur.com/IQJPv4q")
+card4 = Card.create(card_image:"https://imgur.com/ASZopXI")
+card5 = Card.create(card_image:"https://imgur.com/nIn1foD")
+card6 = Card.create(card_image:"https://imgur.com/y29jI62")
+card7 = Card.create(card_image:"https://imgur.com/wl35D0m")
+card8 = Card.create(card_image:"https://imgur.com/CvSUbkv")
 
